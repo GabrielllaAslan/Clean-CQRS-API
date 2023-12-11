7e30982919612c8f942106997bc08380cecedc46
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2023-12-11 14:28:35 +0100

    connecting to db

---
+++ Infrastructure/Database/RealDatabase.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Database
+{
+    public class RealDatabase :DbContext
+    {
+        public RealDatabase() { }
+
+        public RealDatabase(DbContextOptions<RealDatabase> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Dog> Dogs { get; set; }
+        /*
+        public DbSet<Cat> Cats { get; set; }
+        public DbSet<Bird> Birds { get; set; }
+        */
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-O7CREI7D; Database=claen-api-database; Trusted_Connection=True; TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
