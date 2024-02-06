14985bd62e73e40512f50da4728a009187901adb
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-02-06 20:36:44 +0100

    Fixed some misses.

--- API/Controllers/DogsController/DogsController.cs
+++ API/Controllers/DogsController/DogsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Commands.Dogs;
+using Application.Commands.Dogs.DeleteDog;
 using Application.Commands.Dogs.UpdateDog;
 using Application.Dtos;
 using Application.Queries.Dogs.GetAll;
@@ -53,7 +54,15 @@ namespace API.Controllers.DogsController
             return Ok(await _mediator.Send(new UpdateDogByIdCommand(updatedDog, updatedDogId)));
         }
 
-        // IMPLEMENT DELETE !!!
+        [HttpDelete]
+        [Route("deleteDog/{deleteDogId}")]
+        public async Task<IActionResult> DeleteDog(Guid deleteDogId)
+        {
+            await _mediator.Send(new DeleteDogByIdCommand(deleteDogId));
+
+            return NoContent();
+        }
+
 
     }
 }
