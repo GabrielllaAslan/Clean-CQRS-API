e4d1b5419fb011e417a14580ca2ed00da279a372
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-02-07 20:26:28 +0100

    Added cat to the project.

---
+++ Application/Queries/Cats/GetAll/GetAllCatsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+namespace Application.Queries.Cats.GetAll
+{
+    internal sealed class GetAllCatsQueryHandler : IRequestHandler<GetAllCatsQuery, List<Cat>>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public GetAllCatsQueryHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        Task<List<Cat>> IRequestHandler<GetAllCatsQuery, List<Cat>>.Handle(GetAllCatsQuery request, CancellationToken cancellationToken)
+        {
+            List<Cat> allCatsFromMockDatabase = _mockDatabase.Cats;
+            return Task.FromResult(allCatsFromMockDatabase);
+        }
+    }
+}
