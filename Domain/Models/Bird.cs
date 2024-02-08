70b446604d745e1de4af3ec959d2a7922f26906e
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-02-08 21:32:01 +0100

    Fixed bird dto.

--- Domain/Models/Bird.cs
+++ Domain/Models/Bird.cs
@@ -1,13 +1,9 @@
 ﻿using Domain.Models.Animal;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Models
 {
-    [Table("Bird")]
     public class Bird : AnimalModel
     {
         public bool CanFly { get; set; }
-
-        public required string Color { get; set; }
     }
 }
